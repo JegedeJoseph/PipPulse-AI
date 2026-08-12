4cf47ee87ca66e1bf07d9beef5edca469209191b
Joseph Oluwapelumi Jegede <150534104+JegedeJoseph@users.noreply.github.com>
2026-08-12 17:07:19 +0100

    Update engine.py

--- backend/app/sentiment/engine.py
+++ backend/app/sentiment/engine.py
@@ -10,6 +10,8 @@ import torch
 import transformers.utils.import_utils
 transformers.utils.import_utils._torch_available = True
 transformers.utils.import_utils._is_torch = True
+transformers.utils.import_utils.is_torch_available = lambda *args, **kwargs: True
+transformers.utils.import_utils.requires_backends = lambda *args, **kwargs: None
 
 from typing import List, Dict, Any, Optional, Tuple
 from datetime import datetime
